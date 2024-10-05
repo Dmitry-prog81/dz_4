d3b539fa84e55945d4300e974e2dd8c2c49a0547
Dmitry-prog81 <dmitryperevozchikov@icloud.com>
2024-10-05 21:37:49 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+the_number_of_completed_dz=12
+numbers_of_hours_spent=1.5
+course_name="Python"
+time_for_one_task=numbers_of_hours_spent/the_number_of_completed_dz
+print("Курс:",course_name,",","всего задач:",the_number_of_completed_dz,",","затрачено часов:",numbers_of_hours_spent,",",'среднее время выполнения',time_for_one_task,"часа")
